10d74b5384d468ec9570964aa7ecb5c17db82ebd
Francisco Yirá <francisco.yira@outlook.com>
2025-08-06 20:57:38 -0400

    add port

--- app.py
+++ app.py
@@ -241,4 +241,6 @@ def update_markers_and_info(selected_types):
     return filtered, info_text
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8050))
+    app.run(debug=False, host='0.0.0.0', port=port)
